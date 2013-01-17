d9cd4a8e159256f41548d98578309a517d8da835
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2013-01-17 10:47:27 +0100

    Refactoring

--- lib/recipes/base.rb
+++ lib/recipes/base.rb
@@ -7,18 +7,19 @@ Capistrano::Configuration.instance.load do
       install_ruby
       pam_ssh_agent_auth
     end
-    
+
     desc "Init environment for deployment user"
     task :init_deploy_user do
       set :deploy_user, "#{user}"
       with_user "#{sudo_user}" do
         if capture( "#{sudo} cat /etc/passwd | grep #{deploy_user} | wc -l" ).to_i == 0
           run "#{sudo} addgroup admin"
-          run "#{sudo} useradd deployer -m -s /bin/bash -g admin"
+          run "#{sudo} useradd #{deploy_user} -m -s /bin/bash -g admin"
           upload "#{Dir.home}/.ssh/id_rsa.pub", "/tmp/id_rsa.pub"
           run "#{sudo} mkdir -p /home/#{deploy_user}/.ssh"
           run "echo \"cat /tmp/id_rsa.pub >> /home/#{deploy_user}/.ssh/authorized_keys\" | sudo -s"
           run "rm /tmp/id_rsa.pub"
+          run "#{sudo} chown -R #{deploy_user}:admin /home/#{deploy_user}"
         end
       end
     end
@@ -36,20 +37,23 @@ Capistrano::Configuration.instance.load do
 
     desc "Installs up-to-date ruby version from sources"
     task :install_ruby do
-      version = '1.9.3'
-      patch = 'p286'
+      version = (defined? ruby_version) ? ruby_version : '1.9.3'
+      patch = (defined? ruby_patch) ? ruby_patch : 'p362'
       with_user "#{sudo_user}" do
-        if capture( "dpkg --list ruby-#{version} 2>&1 | grep 'No packages found' | wc -l" ).to_i == 1
+        if capture( "dpkg --list ruby-#{version}#{patch} 2>&1 | grep 'No packages found' | wc -l" ).to_i == 1
           run [
             "wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{version}-#{patch}.tar.gz --quiet",
             "tar -xzf ruby-#{version}-#{patch}.tar.gz",
             "cd ruby-#{version}-#{patch}/",
             "./configure --prefix=/usr/local 1>/dev/null",
-            "make 1>/dev/null 2>&1",
-            "#{sudo} checkinstall --default --pkgversion=#{version} --nodoc"
+            "make 1>/dev/null 2>&1"
+          ].join( '&&' )
+          run [
+            "cd ruby-#{version}-#{patch}/",
+            "#{sudo} checkinstall --default --pkgname=ruby-#{version}#{patch} --pkgversion=#{version}#{patch} --nodoc 1>/dev/null 2>&1"
           ].join( '&&' )
           run "#{sudo} gem install bundler --no-ri --no-rdoc"
-          run "#{sudo} gem install chef ruby-shadow --no-ri --no-rdoc"
+          run "#{sudo} gem install chef ruby-shadow right_aws --no-ri --no-rdoc"
           run "#{sudo} gem install server_maint --no-ri --no-rdoc"
         end
       end
@@ -57,16 +61,19 @@ Capistrano::Configuration.instance.load do
 
     desc "Make shared keys and sudo work together"
     task :pam_ssh_agent_auth do
-      version = '0.9.4'
+      version = (defined? pam_ssh_agent_auth_version) ? pam_ssh_agent_auth : '0.9.4'
       with_user "#{sudo_user}" do
-        if capture( "dpkg --list pam-ssh-agent-auth 2>&1 | grep 'No packages found' | wc -l" ).to_i == 1
+        if capture( "dpkg --list pam-ssh-agent-auth-#{version} 2>&1 | grep 'No packages found' | wc -l" ).to_i == 1
           run [
             "wget http://downloads.sourceforge.net/project/pamsshagentauth/pam_ssh_agent_auth/v#{version}/pam_ssh_agent_auth-#{version}.tar.bz2 --quiet",
             "tar -xjf pam_ssh_agent_auth-#{version}.tar.bz2",
             "cd pam_ssh_agent_auth-#{version}",
             "./configure --libexecdir=/lib/security --with-mantype=man 1>/dev/null",
-            "make 1>/dev/null 2>&1",
-            "#{sudo} checkinstall --default --nodoc"
+            "make 1>/dev/null 2>&1"
+          ].join( '&&' )
+          run [
+            "cd pam_ssh_agent_auth-#{version}",
+            "#{sudo} checkinstall --default --pkgname=pam_ssh_agent_auth-#{version} --nodoc 1>/dev/null 2>&1"
           ].join( '&&' )
           pam_config = [
             '#%PAM-1.0',
--- lib/recipes/chef.rb
+++ lib/recipes/chef.rb
@@ -4,36 +4,36 @@ Capistrano::Configuration.instance.load do
   namespace :chef do
     desc "Manages all system dependencies for this application"
     task :manages, roles: :web do
-      run "mkdir -p /home/#{user}/apps/#{application}/shared/log"
-      chef_cookbook_path = 
+      run "mkdir -p /home/#{user}/chef/#{application}/log"
+      chef_cookbook_path =
         capture( %q{ruby -e 'require "server_maint"; puts ServerMaint::get_cookbook_path'} ).chomp
-      app_cookbook_path = "/home/#{user}/apps/#{application}/shared/cookbooks"
+      app_cookbook_path = "/home/#{user}/chef/#{application}/cookbooks"
       chef_config = [
         %Q(cookbook_path ["#{chef_cookbook_path}", File.expand_path("../cookbooks", __FILE__)]),
         %Q(json_attribs File.expand_path("../node.json", __FILE__))
       ].join( "\n" )
       put(
         chef_config,
-        "/home/#{user}/apps/#{application}/shared/solo.rb"
+        "/home/#{user}/chef/#{application}/solo.rb"
       )
       node_config = {
         "user" => "#{user}",
         "application" => "#{application}",
-        "server_name" => "#{host_name}", 
-        "run_list" => ["recipe[main]"] 
+        "server_name" => "#{host_name}",
+        "run_list" => ["recipe[main]"]
       }
-      node_config.update( app_node_config ) if defined? :app_node_config
-      node_config.update( stage_node_config ) if defined? :stage_node_config
-      put node_config.to_json,  "/home/#{user}/apps/#{application}/shared/node.json"
+      node_config.update( app_node_config ) if defined? app_node_config
+      node_config.update( stage_node_config ) if defined? stage_node_config
+      put node_config.to_json,  "/home/#{user}/chef/#{application}/node.json"
       upload(
         "#{cookbooks}",
-        "/home/#{user}/apps/#{application}/shared/",
+        "/home/#{user}/chef/#{application}",
         {:recursive => true, :via => :scp}
       )
       run [
         "#{sudo} chef-solo",
-        "-c /home/#{user}/apps/#{application}/shared/solo.rb",
-        "-L /home/#{user}/apps/#{application}/shared/log/chef.log",
+        "-c /home/#{user}/chef/#{application}/solo.rb",
+        "-L /home/#{user}/chef/#{application}/log/chef.log",
         "1>/dev/null"
       ].join( ' ' )
     end
